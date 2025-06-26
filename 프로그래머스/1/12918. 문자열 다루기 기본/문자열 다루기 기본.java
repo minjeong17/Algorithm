12c06110e86b6daf1c3d42087e436e87185f46eb
minjeong <alswjd35791@naver.com>
2025-06-27 01:22:33 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.09 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        
+        if ((s.length() == 4 || s.length() == 6)) {
+            if (s.matches("\\d+")) return true;
+        }
+        
+        return answer;
+    }
+}

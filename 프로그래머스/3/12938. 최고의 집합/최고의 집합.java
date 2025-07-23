290a86d656b2783daa50c90e1b4abe8302ee51ed
minjeong <alswjd35791@naver.com>
2025-07-23 23:40:38 +0900

    [level 3] Title: 최고의 집합, Time: 0.35 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        
+        if (n > s) return new int[] {-1};
+        
+        int[] answer = new int[n];
+        int n1 = s / n;
+        int n2 = s % n;
+        
+        Arrays.fill(answer, n1);
+        for (int i = 0; i < n2; i++) {
+            answer[n - i - 1] = n1 + 1;
+        }
+        
+        return answer;
+    }
+}

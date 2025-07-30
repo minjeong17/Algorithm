576b6c54b0a776ae6d22bdb5872b2c49b2f599dd
minjeong <alswjd35791@naver.com>
2025-07-30 23:54:33 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.48 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,105 +1,53 @@
 import java.util.*;
 
 class Solution {
-    
-    char[][] keypad = {{'1', '2', '3'}, {'4', '5', '6'},
-                       {'7', '8', '9'}, {'*', '0', '#'}};
-    
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-    
-    public class Node {
-        char hDir;
-        int r;
-        int c;
-        public Node(char hDir, int r, int c) {
-            this.hDir = hDir;
-            this.r = r;
-            this.c = c;
-        }
-    }
-    
     public String solution(int[] numbers, String hand) {
         StringBuilder answer = new StringBuilder();
         
-        Map<Integer, Node> map = new HashMap<>();
-        map.put(1, new Node('L', 0, 0)); map.put(4, new Node('L', 1, 0)); map.put(7, new Node('L', 2, 0));
-        map.put(3, new Node('R', 0, 2)); map.put(6, new Node('R', 1, 2)); map.put(9, new Node('R', 2, 2));
-        map.put(2, new Node('-', 0, 1)); map.put(5, new Node('-', 1, 1)); map.put(8, new Node('-', 2, 1)); map.put(0, new Node('-', 3, 1));
-         
-        int[] leftPos = {3, 0};
-        int[] rightPos = {3, 2};
+        Map<Integer, int[]> numPos = new HashMap<>();
+        numPos.put(1, new int[] {0, 0}); numPos.put(2, new int[] {0, 1}); numPos.put(3, new int[] {0, 2}); 
+        numPos.put(4, new int[] {1, 0}); numPos.put(5, new int[] {1, 1}); numPos.put(6, new int[] {1, 2}); 
+        numPos.put(7, new int[] {2, 0}); numPos.put(8, new int[] {2, 1}); numPos.put(9, new int[] {2, 2}); 
+        numPos.put(0, new int[] {3, 1});
         
-        for (int i = 0; i < numbers.length; i++) {
-            Node node = map.get(numbers[i]);
-            if (node.hDir != '-') {
-                answer.append(node.hDir);
-                if (node.hDir == 'L') {
-                    leftPos[0] = node.r;
-                    leftPos[1] = node.c;
-                } else {
-                    rightPos[0] = node.r;
-                    rightPos[1] = node.c;
-                }
-                
+        int[] left = new int[] {3, 0};
+        int[] right = new int[] {3, 2};
+        for (int n : numbers) {
+            if (n == 1 || n == 4 || n == 7) {
+                answer.append('L');
+                left[0] = n / 3;
+                left[1] = 0;
+            } else if (n == 3 || n == 6 || n == 9) {
+                answer.append('R');
+                right[0] = (n - 3) / 3;
+                right[1] = 2;
             } else {
-                int leftDir = findDis(leftPos, (char) (numbers[i]+'0'));
-                int rightDir = findDis(rightPos, (char) (numbers[i]+'0'));
+                int[] pos = numPos.get(n);
+                int tmpLeft = Math.abs(pos[0] - left[0]) + Math.abs(pos[1] - left[1]);
+                int tmpRight = Math.abs(pos[0] - right[0]) + Math.abs(pos[1] - right[1]);
                 
-                if (leftDir < rightDir) {
+                if (tmpLeft < tmpRight) {
                     answer.append('L');
-                    leftPos[0] = node.r;
-                    leftPos[1] = node.c;
-                } else if (leftDir > rightDir) {
+                    left[0] = pos[0];
+                    left[1] = pos[1];
+                } else if (tmpRight < tmpLeft) {
                     answer.append('R');
-                    rightPos[0] = node.r;
-                    rightPos[1] = node.c;
+                    right[0] = pos[0];
+                    right[1] = pos[1];
                 } else {
                     if ("left".equals(hand)) {
                         answer.append('L');
-                        leftPos[0] = node.r;
-                        leftPos[1] = node.c;
+                        left[0] = pos[0];
+                        left[1] = pos[1];
                     } else {
                         answer.append('R');
-                        rightPos[0] = node.r;
-                        rightPos[1] = node.c;
+                        right[0] = pos[0];
+                        right[1] = pos[1];                        
                     }
                 }
             }
         }
         
         return answer.toString();
-    }
-    
-    public int findDis(int[] pos, char target) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(pos);
-        boolean[][] visited = new boolean[4][3];
-        visited[pos[0]][pos[1]] = true;
-        
-        int dis = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] curr = q.poll();
-                
-                if (keypad[curr[0]][curr[1]] == target) return dis;
-                
-                for (int d = 0; d < 4; d++) {
-                    int nr = curr[0] + dr[d];
-                    int nc = curr[1] + dc[d];
-                    
-                    if (nr < 0 || nr >= 4 || nc < 0 || nc >= 3) continue;
-                    if (visited[nr][nc]) continue;
-                    
-                    q.add(new int[] {nr, nc});
-                    visited[nr][nc] = true;
-                }
-            }
-            
-            dis++;
-        }
-        
-        return dis;
     }
 }

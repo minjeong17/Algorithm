cb28e9edb675ff211f4f89c59ea0036e28905ac0
minjeong <alswjd35791@naver.com>
2025-08-18 02:25:27 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Bronze/2393. Rook/Rook.java
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  ___  ___  ___");
-        System.out.println("  | |__| |__| |");
-        System.out.println("  |           |");
-        System.out.println("   \\_________/");
-        System.out.println("    \\_______/");
-        System.out.println("     |     |");
-        System.out.println("     |     |");
-        System.out.println("     |     |");
-        System.out.println("     |     |");
-        System.out.println("     |_____|");
-        System.out.println("  __/       \\__");
-        System.out.println(" /             \\");
-        System.out.println("/_______________\\");
+        System.out.println("""
+                  ___  ___  ___
+                  | |__| |__| |
+                  |           |
+                   \\_________/
+                    \\_______/
+                     |     |
+                     |     |
+                     |     |
+                     |     |
+                     |_____|
+                  __/       \\__
+                 /             \\
+                /_______________\\
+                """);
+
     }
 }

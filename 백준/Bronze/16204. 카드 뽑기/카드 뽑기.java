4ec9f21c56569c1ce978a31e0facb05108437fdf
minjeong <alswjd35791@naver.com>
2026-01-12 19:18:01 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        System.out.println(Math.min(M, K) + Math.min(N-M, N-K));
+    }
+}

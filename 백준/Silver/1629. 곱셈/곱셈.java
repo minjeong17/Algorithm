733652f090ef54b5e71f16a06288f7be3180317a
minjeong <alswjd35791@naver.com>
2026-01-12 18:53:32 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,36 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.math.*;
 
-public class Main {
-	
-	static int C;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		long ans = pow(A, B); 
-		
-		System.out.println(ans);
-	}
-
-	private static long pow(long a, long b) {
-		
-		if (b == 0) return 1;
-		
-		long tmp = pow(a, b/2);
-		long res = (tmp * tmp) % C;
-		
-		if (b % 2 == 1) {
-			res = (res * a) % C;
-		}
-		
-		return res;
-	}
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger C = new BigInteger(st.nextToken());
+        
+        System.out.println(A.modPow(B, C));
+    }
 }

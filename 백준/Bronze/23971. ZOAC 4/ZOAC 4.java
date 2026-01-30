73059f6c9f3faf47d0c2c2ffc5ac595db24bd015
minjeong <alswjd35791@naver.com>
2026-01-30 15:32:10 +0900

    [Bronze III] Title: ZOAC 4, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -1,8 +1,8 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Main {
-    public static void main(String[] args) throws Exception {
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
@@ -10,13 +10,8 @@ class Main {
         int W = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        
-        int vert = H/(N+1);
-        int hori = W/(M+1);
-        
-        if (H % (N+1) != 0) vert++;
-        if (W % (M+1) != 0) hori++;
-        
-        System.out.println(vert * hori);
+
+        int answer = (int)Math.ceil((double)H/(N+1)) * (int)Math.ceil(((double)W/(M+1)));
+        System.out.println(answer);
     }
 }

33447cfd4eab2edc16458886ee93c216f1210c7e
minjeong <alswjd35791@naver.com>
2026-02-01 18:31:10 +0900

    [Bronze I] Title: 디지털 티비, Time: 96 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.java
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.java
@@ -1,47 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
         
-        StringBuilder sb = new StringBuilder();
-        
-        String[] channels = new String[N];
-        int KBS1Pos = 0;
-        int KBS2Pos = 0;
-        for(int i=0; i<N; i++) {
-            String currChannel = br.readLine();
-            if(currChannel.equals("KBS1")) KBS1Pos = i;
-            else if(currChannel.equals("KBS2")) KBS2Pos = i;
-        }
-        boolean flag = false;
-	if (KBS1Pos > KBS2Pos) flag = true;
-
-        int pointer = 0;
-        while (KBS1Pos > pointer) {
-            sb.append("1");
-            pointer++;
+        int num1 = -1;
+        int num2 = -1;
+        for (int i = 0; i < N; i++) {
+            String ch = br.readLine();
+            if ("KBS1".equals(ch)) {
+                num1 = i;
+            } else if ("KBS2".equals(ch)) {
+                num2 = i;
+            }
         }
         
-        while (KBS1Pos > 0) {
-            sb.append("4");
-            KBS1Pos--;
-            pointer--;
+        StringBuilder ans = new StringBuilder();
+        if (num1 < num2) {
+            for (int i = 0; i < num2; i++) {
+                ans.append("1");
+            }
+            for (int i = 0; i < num2; i++) {
+                ans.append("4");
+            }
+            for (int i = 0; i < num1+1; i++) {
+                ans.append("1");
+            }
+            for (int i = 0; i < num1+1; i++) {
+                ans.append("4");
+            }
+        } else {
+            for (int i = 0; i < num2; i++) {
+                ans.append("1");
+            }
+            for (int i = 0; i < num2; i++) {
+                ans.append("4");
+            }
+            for (int i = 0; i < num1; i++) {
+                ans.append("1");
+            }
+            for (int i = 0; i < num1; i++) {
+                ans.append("4");
+            }
         }
         
-	if (flag) KBS2Pos++;
-        while (KBS2Pos > pointer) {
-            sb.append("1");
-            pointer++;
-        }
         
-        while (KBS2Pos > 1) {
-            sb.append("4");
-            KBS2Pos--;
-            pointer--;
-        }
-        System.out.println(sb);
+        System.out.println(ans.toString());
     }
 }

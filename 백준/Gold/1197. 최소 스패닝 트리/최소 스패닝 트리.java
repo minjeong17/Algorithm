a8a9e4edc52756c4d1739e848d4c64c75f0e8f18
minjeong <alswjd35791@naver.com>
2026-04-03 23:05:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 544 ms, Memory: 54992 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,75 +4,60 @@ import java.util.*;
 public class Main {
     
     static class Edge implements Comparable<Edge> {
-        int from;
         int to;
         int weight;
         
-        public Edge(int from, int to, int weight) {
-            this.from = from;
+        public Edge(int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
         
         @Override
         public int compareTo(Edge o) {
-            return this.weight - o.weight;
+            return Integer.compare(this.weight, o.weight);
         }
-    } 
+    }
     
-    static int V;
-    static int[] p;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        V = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         
-        Edge[] edges = new Edge[E];
+        List<Edge>[] adj = new ArrayList[V+1];
+        for (int i = 1; i <= V; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
             
-            edges[i] = new Edge(from, to, weight);
+            adj[from].add(new Edge(to, weight));
+            adj[to].add(new Edge(from, weight));
         }
         
-        Arrays.sort(edges);
-        
-        makeSet();
-        
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[V+1];
+        pq.add(new Edge(1, 0));
         int pick = 0;
-        int cost = 0;
-        for (Edge edge : edges) {
-            if (unionSet(edge.from, edge.to)) {
-                cost += edge.weight;
-                if (++pick == V - 1) break;
+        
+        int ans = 0;
+        while (pick < V && !pq.isEmpty()) {
+            Edge e = pq.poll();
+            if (visited[e.to]) continue;
+            
+            ans += e.weight;
+            visited[e.to] = true;
+            pick++;
+            for (Edge ed : adj[e.to]) {
+                if (!visited[ed.to]) pq.add(ed);
             }
         }
         
-        System.out.println(cost);
+        System.out.println(ans);
     }
-    
-    static void makeSet() {
-        p = new int[V+1];
-        for (int i = 1; i <= V; i++) {
-            p[i] = i;
-        }
-    }
-    
-    static int findSet(int v) {
-        if (v == p[v]) return v;
-        return p[v] = findSet(p[v]);
-    }
-    
-    static boolean unionSet(int v1, int v2) {
-        v1 = findSet(v1);
-        v2 = findSet(v2);
-        if (v1 == v2) return false;
-        
-        p[v1] = v2;
-        return true;
-    }
-}
+} 

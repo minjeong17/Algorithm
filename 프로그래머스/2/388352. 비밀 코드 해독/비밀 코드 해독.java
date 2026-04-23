de7789cf0528c8c5147493ca13fc8036e2050590
minjeong <alswjd35791@naver.com>
2026-04-23 22:29:10 +0900

    [level 2] Title: 비밀 코드 해독, Time: 17.54 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,54 +1,103 @@
 import java.util.*;
 
 class Solution {
-    public int[] sel;
-    public List<int[]> list;
+    public int answer;
+    public int[] info, sel;
     public int solution(int n, int[][] q, int[] ans) {
-        int answer = 0;
+        answer = 0;
         
-        list = new ArrayList<>();
-        sel = new int[5];
-        comb(1, 0, n);
-        
-        List<Set<Integer>> qList = new ArrayList<>();
-        for (int[] qq : q) {
-            Set<Integer> set = new HashSet<>();
-            for (int qqq : qq) {
-                set.add(qqq);
+        info = new int[q.length];
+        for (int i = 0; i < q.length; i++) {
+            int tmp = 0;
+            for (int qq : q[i]) {
+                tmp |= (1 << qq);
             }
-            
-            qList.add(set);
-        } 
-        
-                
-        for (int i = 0; i < list.size(); i++) {
-            int[] curr = list.get(i);
-            
-            boolean flag = true;
-            for (int j = 0; j < q.length; j++) {
-                int cnt = 0;
-                for (int k = 0; k < 5; k++) {
-                    if (qList.get(j).contains(curr[k])) cnt++;
-                }
-                
-                if (cnt != ans[j]) flag = false;
-            }
-            
-            if (flag) answer++;
+            info[i] = tmp;
         }
+        
+        sel = new int[5];
+        comb(1, 0, n, ans);
         
         return answer;
     }
     
-    public void comb(int idx, int sidx, int n) {
+    public void comb(int idx, int sidx, int n, int[] ans) {
         if (sidx == 5) {
-            list.add(sel.clone());
+            int selBit = 0;
+            for (int s : sel) {
+                selBit |= (1 << s);
+            }
+            
+            boolean flag = true;
+            for (int i = 0; i < ans.length; i++) {
+                if (Integer.bitCount(info[i] & selBit) != ans[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) answer++;
             return;
         }
         
         for (int i = idx; i <= n; i++) {
             sel[sidx] = i;
-            comb(i+1, sidx+1, n);
+            comb(i+1, sidx+1, n, ans);
         }
     }
 }
+
+
+// 조합 + 완잔탐색
+// class Solution {
+//     public int[] sel;
+//     public List<int[]> list;
+//     public int solution(int n, int[][] q, int[] ans) {
+//         int answer = 0;
+        
+//         list = new ArrayList<>();
+//         sel = new int[5];
+//         comb(1, 0, n);
+        
+//         List<Set<Integer>> qList = new ArrayList<>();
+//         for (int[] qq : q) {
+//             Set<Integer> set = new HashSet<>();
+//             for (int qqq : qq) {
+//                 set.add(qqq);
+//             }
+            
+//             qList.add(set);
+//         } 
+        
+                
+//         for (int i = 0; i < list.size(); i++) {
+//             int[] curr = list.get(i);
+            
+//             boolean flag = true;
+//             for (int j = 0; j < q.length; j++) {
+//                 int cnt = 0;
+//                 for (int k = 0; k < 5; k++) {
+//                     if (qList.get(j).contains(curr[k])) cnt++;
+//                 }
+                
+//                 if (cnt != ans[j]) flag = false;
+//             }
+            
+//             if (flag) answer++;
+//         }
+        
+//         return answer;
+//     }
+    
+//     public void comb(int idx, int sidx, int n) {
+//         if (sidx == 5) {
+//             list.add(sel.clone());
+//             return;
+//         }
+        
+//         for (int i = idx; i <= n; i++) {
+//             sel[sidx] = i;
+//             comb(i+1, sidx+1, n);
+//         }
+//     }
+// }

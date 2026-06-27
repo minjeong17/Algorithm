b0c7dc4d624a0430b7235971257ab36b9c84f4fd
minjeong <alswjd35791@naver.com>
2026-06-27 17:38:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 9.27 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,49 +3,40 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
         
-        Map<Integer, Integer> carNum = new HashMap<>();
-        Map<Integer, Integer> carTime = new HashMap<>();
+        Map<String, Integer> inOut = new HashMap<>();
+        Map<String, Integer> totalTime = new TreeMap<>();
         for (String record : records) {
-            String[] rec = record.split(" ");
-            String time = rec[0];
-            int num = Integer.parseInt(rec[1]);
+            String[] r = record.split(" ");
+            String carNum = r[1];
+            String[] t = r[0].split(":");
+            int time = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
             
-            if (rec[2].equals("IN")) {
-                String[] t = time.split(":");
-                int timeIn = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
-                carNum.put(num, timeIn);
+            if (r[2].equals("IN")) {
+                inOut.put(carNum, time);
             } else {
-                String[] t = time.split(":");
-                int timeOut = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+                int inTime = inOut.get(carNum);
+                totalTime.put(carNum, totalTime.getOrDefault(carNum, 0) + (time - inTime));
                 
-                int totalT = timeOut - carNum.get(num);
-                carTime.put(num, carTime.getOrDefault(num, 0)+totalT);
-                carNum.remove(num);
+                inOut.remove(carNum);
             }
         }
         
-        int endTime = 23 * 60 + 59;
-        for (int cNum : carNum.keySet()) {
-            int totalT = endTime - carNum.get(cNum);
-            carTime.put(cNum, carTime.getOrDefault(cNum, 0) + totalT);
+        if (inOut.size() > 0) {
+            for (String carNum : inOut.keySet()) {
+                int inTime = inOut.get(carNum);
+                totalTime.put(carNum, totalTime.getOrDefault(carNum, 0) + ((23*60+59) - inTime));
+            }
+        }
+        
+        int[] answer = new int[totalTime.size()];
+        int idx = 0;
+        for (String carNum : totalTime.keySet()) {
+            int tmp = (int)Math.ceil((totalTime.get(carNum) - fees[0]) / (double)fees[2]);
+            
+            if (tmp >= 0) answer[idx++] = fees[1] + tmp * fees[3];
+            else answer[idx++] = fees[1];
         }
 
-        TreeMap<Integer, Integer> carPay = new TreeMap<>();
-        for (int cNum : carTime.keySet()) {
-            int pay = fees[1];
-            int totalT = carTime.get(cNum);
-            if (totalT > fees[0]) {
-                pay += Math.ceil((totalT - fees[0])/(double)fees[2]) * fees[3];
-            } 
-            carPay.put(cNum, pay);
-        }
-        
-        int[] answer = new int[carPay.size()];
-        int idx = 0;
-        for (int cNum : carPay.keySet()) {
-            answer[idx++] = carPay.get(cNum);
-        }
-        
         return answer;
     }
 }

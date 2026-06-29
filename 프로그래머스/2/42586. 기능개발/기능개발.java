27e8e11100502bc9f296efbf07b58fc818e6b252
minjeong <alswjd35791@naver.com>
2026-06-29 18:02:35 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,32 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        List<Integer> tmp = new ArrayList<>();
-        
-        int[] left = new int[speeds.length];
-        for (int i = 0; i < left.length; i++) {
-            int t = (100 - progresses[i]) / speeds[i];
-            left[i] = (100 - progresses[i]) % speeds[i] == 0 ? t : t + 1;
+        int[] time = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            time[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
         
-        int count = 1;
-        int prev = left[0];
-        for (int i = 1; i < left.length; i++) {
-            if (left[i] <= prev) {
-                count++;
+        List<Integer> tmpAns = new ArrayList<>();
+        int cnt = 1;
+        int prev = time[0];
+        for (int i = 1; i < progresses.length; i++) {
+            if (time[i] <= prev) {
+                cnt++;
             } else {
-                tmp.add(count);
-                count = 1;
-                prev = left[i];
+                tmpAns.add(cnt);
+                cnt = 1;
+                prev = time[i];
             }
         }
-
-        tmp.add(count); 
+        tmpAns.add(cnt);
         
-        
-        int[] answer = new int[tmp.size()];
-        for (int i = 0; i < tmp.size(); i++) {
-            answer[i] = tmp.get(i);
+        int[] answer = new int[tmpAns.size()];
+        for (int i = 0; i < tmpAns.size(); i++) {
+            answer[i] = tmpAns.get(i);
         }
         
         return answer;

b233ede0cbfc6a16cb5ade3951927e44ed3e1f9f
minjeong <alswjd35791@naver.com>
2026-06-29 18:20:14 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int tmp = (int) Math.sqrt((4-brown) * (4-brown) - 16 * yellow);
-        int x = 4 - brown > 0 ? ((-1) * (4 - brown) + tmp) / 4 : (-(4 - brown) - tmp) / 4;
-        int y = yellow / x;
+        int m = brown + yellow;
+        int s = (m + 4 - yellow) / 2;
         
-        answer[0] = Math.max(x, y)+2;
-        answer[1] = Math.min(x, y)+2;
+        answer[0] = (s + (int)Math.sqrt(s * s - 4 * m)) / 2;
+        answer[1] = (s - (int)Math.sqrt(s * s - 4 * m)) / 2;
         
         return answer;
     }

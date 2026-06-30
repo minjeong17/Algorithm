ee2784d4c2879fb70ab48607006cb6ce7e5829ae
minjeong <alswjd35791@naver.com>
2026-06-30 15:01:07 +0900

    [level 2] Title: 타겟 넘버, Time: 4.86 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,41 +1,24 @@
-import java.util.*;
-
 class Solution {
-    
-    char[] opList;
-    char[] op = {'+', '-'};
-    int size, ans;
-    
+    int[] numbers;
+    int target, answer;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        this.numbers = numbers;
+        this.target = target;
         
-        size = numbers.length;
-        opList = new char[size];
-        ans = 0;
-        comb(0, 0, numbers, target);
+        answer = 0;
         
-        return ans;
+        dfs(0, 0);
+        
+        return answer;
     }
     
-    public void comb(int idx, int sidx, int[] numbers, int target) {
-        if (sidx == size) {
-            cal(opList, numbers, target);
+    public void dfs(int idx, int sum) {        
+        if (idx == numbers.length) {
+            if (sum == target) answer++;
             return;
-        }
+        } 
         
-        for (int i = idx; i < 2; i++) {
-            opList[sidx] = op[i];
-            comb(idx, sidx+1, numbers, target);
-        }
-    }
-    
-    public void cal(char[] opList, int[] numbers, int target) {
-        int tmp = 0;
-        for (int i = 0; i < size; i++) {
-            if (opList[i] == '+') tmp += numbers[i];
-            else tmp -= numbers[i];
-        }
-        
-        if (tmp == target) ans++;
+        dfs(idx+1, sum+numbers[idx]);
+        dfs(idx+1, sum-numbers[idx]);
     }
 }

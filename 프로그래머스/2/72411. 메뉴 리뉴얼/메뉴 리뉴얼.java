ec102472305ea679b05a9ce9882f3ce1ca369adc
minjeong <alswjd35791@naver.com>
2026-07-01 20:47:30 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.33 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,58 +1,52 @@
 import java.util.*;
 
 class Solution {
-    
-    Map<String, Integer> cnt;
-    int[] maxLen = new int[11];
-    char[] sel;
-        
+    Map<String, Integer> map;
+    Map<Integer, Integer> cntMap;
+    Character[] sel;
+    char[] order;
     public String[] solution(String[] orders, int[] course) {
         
-        cnt = new HashMap<>();
-        
-        for (int c : course) {
-            for (String order : orders) {
-                sel = new char[c];
-                char[] ch = order.toCharArray();
-                Arrays.sort(ch);
-                comb(0, 0, c, ch);
+        map = new HashMap<>();
+        cntMap = new HashMap<>();
+        for (int i = 0; i < orders.length; i++) {
+            order = orders[i].toCharArray();
+            Arrays.sort(order);
+            for (int c : course) {
+                sel = new Character[c];
+                if (!cntMap.containsKey(c)) cntMap.put(c, 0);
+                comb(0, 0, c);
             }
         }
         
-        List<String> list = new ArrayList<>();
-        
-        for (String str : cnt.keySet()) {
-            int strLen = str.length();
-            if (maxLen[strLen] == cnt.get(str) && cnt.get(str) >= 2) {
-                list.add(str);   
-            }
+        List<String> ansList = new ArrayList<>();
+        for (String cand : map.keySet()) {
+            int size = map.get(cand);
+            if (size >= 2 && size == cntMap.get(cand.length())) ansList.add(cand);
         }
         
-        Collections.sort(list);
+        String[] answer = new String[ansList.size()];
+        for (int i = 0; i < ansList.size(); i++) {
+            answer[i] = ansList.get(i);
+        }
         
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }        
+        Arrays.sort(answer);
         
         return answer;
     }
     
-    public void comb(int idx, int sidx, int c, char[] ch) {
-        if (sidx == c) {
+    public void comb (int idx, int sidx, int c) {
+        if (sidx == sel.length) {
             StringBuilder sb = new StringBuilder();
-            for (char s : sel) {
-                sb.append(s);
-            }
-            String tmp = sb.toString();
-            cnt.put(tmp, cnt.getOrDefault(tmp, 0) + 1);
-            maxLen[c] = Math.max(maxLen[c], cnt.get(tmp));
+            for (char s : sel) sb.append(s);
+            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+            cntMap.put(c, Math.max(cntMap.get(c), map.get(sb.toString())));
             return;
         }
         
-        for (int i = idx; i < ch.length; i++) {
-            sel[sidx] = ch[i];
-            comb(i+1, sidx+1, c, ch);
+        for (int i = idx; i < order.length; i++) {
+            sel[sidx] = order[i];
+            comb(i+1, sidx+1, c);
         }
     }
 }

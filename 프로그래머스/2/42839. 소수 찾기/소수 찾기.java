698be1d9239a272c1845d9743079fb9778470af4
minjeong <alswjd35791@naver.com>
2026-07-01 15:54:56 +0900

    [level 2] Title: 소수 찾기, Time: 204.60 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,68 +1,64 @@
 import java.util.*;
 
 class Solution {
-
-    int[] nums;
+    int answer;
     boolean[] isPrime;
+    int[] nums;
+    int[] tmp;
     boolean[] visited;
-    int len;
-    
-    int[] sel;
-    boolean[] visitedP;
-    
-    int answer = 0;
-    
+    Set<Integer> set;
     public int solution(String numbers) {
+        answer = 0;
         
-        len = numbers.length();
-        nums = new int[len];
-        for (int i = 0; i < len; i++) {
-            nums[i] = Integer.parseInt(numbers.charAt(i)+"");
+        int len = numbers.length();
+        
+        int maxNum = (int)Math.pow(10, len) - 1;
+        isPrime = new boolean[maxNum + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false; isPrime[1] = false;
+        for (int i = 2; i * i <= maxNum; i++) {
+            for (int j = 2; i * j <= maxNum; j++) {
+                isPrime[i*j] = false;
+            }
         }
         
-        int tLen = (int)Math.pow(10, len+1);
-        isPrime = new boolean[tLen];
-        visited = new boolean[tLen];
+        nums = new int[len];
+        for (int i = 0; i < len; i++) {
+            nums[i] = numbers.charAt(i) - '0';
+        }
         
-        Arrays.fill(isPrime, true);
-        isPrime[0] = false;
-        isPrime[1] = false;
-        
-        for (int i = 2; i * i <= tLen; i++) {
-            if (isPrime[i]) {
-                for (int j = i * 2; j < tLen; j+=i) {
-                    isPrime[j] = false;
-                }
-            }
-        } 
-        
+        set = new HashSet<>();
         for (int i = 1; i <= len; i++) {
-            sel = new int[i];
-            visitedP = new boolean[len];
-            perm(0, i);
+            tmp = new int[i];
+            visited = new boolean[len];
+            perm(0);
         }
         
         return answer;
     }
     
-    public void perm(int sidx, int l) {
-        if (sidx == l) {
+    public void perm (int idx) {
+        if (idx == tmp.length) {
             StringBuilder sb = new StringBuilder();
-            for (int s : sel) sb.append(s);
-            int n = Integer.parseInt(sb.toString());
-            if (!visited[n] && isPrime[n]) {
-                visited[n] = true;
-                answer++;
+            for (int i = 0; i < tmp.length; i++) {
+                sb.append(tmp[i]);
             }
+            int n = Integer.parseInt(sb.toString());
+            
+            if (!set.contains(n)) {
+                set.add(n);
+                if (isPrime[n]) answer++;
+            }
+            
             return;
         }
         
-        for (int i = 0; i < len; i++) {
-            if (!visitedP[i]) {
-                visitedP[i] = true;
-                sel[sidx] = nums[i];
-                perm(sidx+1, l);
-                visitedP[i] = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                tmp[idx] = nums[i];
+                visited[i] = true;
+                perm(idx+1);
+                visited[i] = false;
             }
         }
     }

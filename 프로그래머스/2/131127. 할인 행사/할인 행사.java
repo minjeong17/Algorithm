38fa0a5df0ea709b8e5cb2685e968d45909d13ad
minjeong <alswjd35791@naver.com>
2026-07-02 18:26:54 +0900

    [level 2] Title: 할인 행사, Time: 10.97 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,40 +1,51 @@
 import java.util.*;
 
 class Solution {
+    int[] number;
     public int solution(String[] want, int[] number, String[] discount) {
+        this.number = number;
         int answer = 0;
         
-        Map<String, Integer> cnt = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            cnt.put(want[i], number[i]);
+            idxMap.put(want[i], i);
         }
         
-        int sum = 0;
-        for (int n : number) sum += n;
-        
-        Map<String, Integer> tmp = new HashMap<>();
-        
-        for (int i = 0; i <= discount.length - sum; i++) {
-            tmp.putAll(cnt);
+        int[] cnt = new int[want.length];
+        for (int i = 0; i < 10; i++) {
+            String w = discount[i];
             
-            boolean isOk = true;
-            for (int j = i; j < i + sum; j++) {
-                if (!tmp.containsKey(discount[j])) {
-                    isOk = false;
-                    break;
-                } else {
-                    if (tmp.get(discount[j]) == 1) tmp.remove(discount[j]);
-                    else {
-                        tmp.put(discount[j], tmp.get(discount[j])-1);
-                    }
-                }
-            }
-            
-            if (isOk && tmp.size() == 0) {
-                answer++;
+            if (idxMap.containsKey(w)) {
+                int idx = idxMap.get(w);
+                cnt[idx]++;
             }
         }
-         
+        
+        if (check(cnt)) answer++;
+        
+        for (int i = 0; i < discount.length - 10; i++) {
+            if (idxMap.containsKey(discount[i])) {
+                int idx = idxMap.get(discount[i]);
+                cnt[idx]--;
+            }
+            
+            if (idxMap.containsKey(discount[i+10])) {
+                int idx = idxMap.get(discount[i+10]);
+                cnt[idx]++;
+            }
+            
+            if (check(cnt)) answer++;
+        }
+        
+        
         return answer;
+    }
+    
+    public boolean check(int[] cnt) {
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] != number[i]) return false;
+        }
+        
+        return true;
     }
 }

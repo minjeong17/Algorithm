3edb99aa6f9fa48c3b92da922cd208e947d64bd0
minjeong <alswjd35791@naver.com>
2026-07-02 18:11:36 +0900

    [level 2] Title: 괄호 회전하기, Time: 24.90 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -8,44 +8,30 @@ class Solution {
         
         int len = s.length();
         for (int i = 0; i < len; i++) {
-            Stack<Character> stack = new Stack<>();
-            
-            boolean isOk = true;
-            for (int j = i; j < i + len; j++) {
-                char curr = newS.charAt(j);
-                if (curr == '(' || curr == '{' || curr == '[') {
-                    stack.push(curr);
-                } else {
-                    if (stack.isEmpty()) {
-                        isOk = false;
-                        break;
-                    } else {
-                        char p = stack.pop();
-                        if (curr == ')') {
-                            if (p != '(') {
-                                isOk = false;
-                                break;
-                            }
-                        } else if (curr == '}') {
-                            if (p != '{') {
-                                isOk = false;
-                                break;
-                            }
-                        } else {
-                            if (p != '[') {
-                                isOk = false;
-                                break;
-                            }
-                        }
-                    }
-                }
+            if (check(newS.substring(i, i + len))) {
+                answer++;
             }
-            
-            if (isOk && stack.isEmpty()) answer++;
         }
         
-        
-        
         return answer;
+    }
+    
+    public boolean check(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') return false;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            }
+        }
+        
+        if (stack.isEmpty()) return true;
+        else return false;
     }
 }

8ab79bdc3bb4df5a5d183bb258e958b92f5cd264
minjeong <alswjd35791@naver.com>
2026-07-08 19:01:50 +0900

    [level 2] Title: 올바른 괄호, Time: 8.32 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        
+
         Stack<Character> stack = new Stack<>();
-        
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(') stack.push('(');
+            char c = s.charAt(i);
+            
+            if (c == '(') stack.push(c);
             else {
                 if (stack.isEmpty()) return false;
-                else stack.pop();
+                stack.pop();
             }
         }
 
-        return stack.isEmpty() ? true : false;
+        if (stack.isEmpty()) return true;
+        else return false;
     }
 }

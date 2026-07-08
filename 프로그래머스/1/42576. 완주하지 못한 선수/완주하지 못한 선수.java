1ce716f94832dfef47e5433fe932af2157953aa7
minjeong <alswjd35791@naver.com>
2026-07-08 17:47:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.11 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,25 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-
-        Map<String, Integer> comp = new HashMap<>();
-        for (String name : completion) {
-            comp.put(name, comp.getOrDefault(name, 0) + 1);
+        String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
         
-        for (String name : participant) {
-            if (!comp.containsKey(name)) {
-                return name;
-            } else {
-                if (comp.get(name) - 1 == 0) comp.remove(name);
-                else {
-                    comp.put(name, comp.get(name) - 1);
-                }
+        for (String c : completion) {
+            if (map.containsKey(c)) {
+                if (map.get(c) == 1) map.remove(c);
+                else map.put(c, map.get(c) - 1);
             }
         }
         
-        return "";
-    }
-}   
-
+        for (String s : map.keySet()) answer = s;
         
+        return answer;
+    }
+}

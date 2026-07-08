cfcf6f6a9fb1f4bc7a09755bd66d07b20f57ae19
minjeong <alswjd35791@naver.com>
2026-07-08 19:04:09 +0900

    [level 2] Title: NULL 처리하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,7 +1,7 @@
-SELECT ANIMAL_TYPE, 
-       (CASE WHEN NAME IS NULL THEN 'No name'
-        ELSE NAME
-        END) AS NAME, SEX_UPON_INTAKE
+-- 코드를 입력하세요
+SELECT ANIMAL_TYPE, (CASE WHEN NAME IS NULL THEN 'No name'
+                          ELSE NAME
+                     END) AS NAME, SEX_UPON_INTAKE
  FROM ANIMAL_INS
  ORDER BY ANIMAL_ID
 ;

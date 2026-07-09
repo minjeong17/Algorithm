1bd9c3cae20b22ef53c15c9a1a2a4d764e7e54b3
minjeong <alswjd35791@naver.com>
2026-07-09 16:53:24 +0900

    [level 2] Title: 배달, Time: 3.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,53 +1,47 @@
 import java.util.*;
 
 class Solution {
-    
-    int[] len;
+    int N;
     List<int[]>[] adj;
-    
+    int[] dist;
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
+        this.N = N;
 
         adj = new ArrayList[N+1];
         for (int i = 1; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
-        
         for (int[] r : road) {
-            int s = r[0];
-            int e = r[1];
-            int w = r[2];
-            
-            adj[s].add(new int[] {e, w});
-            adj[e].add(new int[] {s, w});
+            adj[r[0]].add(new int[] {r[1], r[2]});
+            adj[r[1]].add(new int[] {r[0], r[2]});
         }
         
-        len = new int[N+1];
-        Arrays.fill(len, Integer.MAX_VALUE);
-        len[1] = 0;
-        find();
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+        dijkstra(1);
         
         for (int i = 1; i <= N; i++) {
-            if (len[i] <= K) answer++;
+            if (dist[i] <= K) answer++;
         }
         
         return answer;
     }
     
-    public void find() {
+    public void dijkstra(int v) {
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
+        q.add(v);
         
         while (!q.isEmpty()) {
             int curr = q.poll();
             
-            for (int[] arr : adj[curr]) {
-                int end = arr[0];
-                int weight = arr[1];
-                
-                if (len[curr] + weight < len[end]) {
-                    len[end] = len[curr] + weight;
-                    q.add(end);
+            for (int[] next : adj[curr]) {
+                int to = next[0];
+                int weight = next[1];
+                if (dist[curr] + weight < dist[to]) {
+                    dist[to] = Math.min(dist[curr] + weight, dist[to]);
+                    q.add(to);
                 }
             }
         }

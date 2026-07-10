b8da072767e562cf589c14ab11795870857d687a
minjeong <alswjd35791@naver.com>
2026-07-10 15:35:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 127.82 ms, Memory: 233 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -4,18 +4,23 @@ class Solution {
     public int solution(int[] topping) {
         int answer = 0;
         
-        Map<Integer, Integer> cntTopping = new HashMap<>();
-        for (int t : topping) cntTopping.put(t, cntTopping.getOrDefault(t, 0) + 1);
+        Map<Integer, Integer> type1 = new HashMap<>();
+        Map<Integer, Integer> type2 = new HashMap<>();
         
-        Set<Integer> hs = new HashSet<>();
-        for (int t : topping) {
-            hs.add(t);
-            if (cntTopping.get(t) == 1) cntTopping.remove(t);
-            else {
-                cntTopping.put(t, cntTopping.get(t)-1);
-            }
+        for (int i = 0; i < topping.length; i++) {
+            type2.put(topping[i], type2.getOrDefault(topping[i], 0) + 1);
+        }
+        
+        int idx = 0;
+        while (type2.size() > 0) {
+            int t = topping[idx];
             
-            if (hs.size() == cntTopping.size()) answer++;
+            type1.put(t, type1.getOrDefault(t, 0) + 1);
+            if (type2.get(t) == 1) type2.remove(t);
+            else type2.put(t, type2.get(t) - 1);
+            
+            if (type1.size() == type2.size()) answer++;
+            idx++;
         }
         
         return answer;

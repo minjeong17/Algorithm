30c8644459538e414207644c1d67a4efa2e1bd12
minjeong <alswjd35791@naver.com>
2026-07-22 16:49:13 +0900

    [level 2] Title: 호텔 대실, Time: 6.44 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = Integer.MIN_VALUE;
+        int answer = 0;
         
-        PriorityQueue<int[]> times = new PriorityQueue<>((o1, o2) -> {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
             if (o1[0] == o2[0]) return o1[1] - o2[1];
             return o1[0] - o2[0];
         });
         
-        for (String[] book : book_time) {
-            String[] start = book[0].split(":");
-            String[] end = book[1].split(":");
+        for (String[] bt : book_time) {
+            String[] s = bt[0].split(":");
+            String[] e = bt[1].split(":");
             
-            int[] tmp = new int[2];
-            tmp[0] = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
-            tmp[1] = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
-            
-            times.add(tmp);
+            pq.add(new int[] {Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]), 
+                              Integer.parseInt(e[0]) * 60 + Integer.parseInt(e[1])});
         }
         
-        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
-        while (!times.isEmpty()) {
-            int[] curr = times.poll();
+        PriorityQueue<Integer> endTime = new PriorityQueue<>();
+        while (!pq.isEmpty()) {
+            int[] t = pq.poll();
             
-            while (!endTimes.isEmpty() && endTimes.peek() <= curr[0]) {
-                endTimes.poll();
+            if (!endTime.isEmpty() && endTime.peek() + 10 <= t[0]) {
+                endTime.poll();
+                endTime.add(t[1]);
+            } else {
+                endTime.add(t[1]);
+                answer++;
             }
-            endTimes.add(curr[1]+10);
-            answer = Math.max(answer, endTimes.size());
-        } 
+        }
+
         
         return answer;
     }

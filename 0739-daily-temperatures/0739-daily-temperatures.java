1c28f3efa95844054ca862af1c23e51b7010b44e
minjeong <alswjd35791@naver.com>
2026-09-02 13:06:12 +0900

    Time: 71 ms (37.73%), Space: 106.4 MB (78.96%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                    stack.pop();
+                }
+
+                if (!stack.isEmpty()) answer[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
